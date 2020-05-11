09a2430a0b7d7d6aad6138434a64c1b4bf0ca109
frankmag <franklin.magoba@medbookafrica.com>
2020-05-11 17:50:45 +0300

    housekeeping

---
+++ build/precache-manifest.7da6d17594ba8b631b47f60d138dc9dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2189b1963df4f28ddcaf1e7515b0ea4b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3ec16a313d38daa4bf48",
+    "url": "/static/css/main.1ff9cbe9.chunk.css"
+  },
+  {
+    "revision": "a24ff46e9226b37927bd",
+    "url": "/static/js/2.b9eacea7.chunk.js"
+  },
+  {
+    "revision": "ef90dec32da5a94b550496958cb808df",
+    "url": "/static/js/2.b9eacea7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ec16a313d38daa4bf48",
+    "url": "/static/js/main.177cafe0.chunk.js"
+  },
+  {
+    "revision": "b8837de9163626b7dc3b",
+    "url": "/static/js/runtime-main.79e1887d.js"
+  }
+]);
